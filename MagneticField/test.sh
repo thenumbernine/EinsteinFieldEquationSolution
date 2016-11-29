db6c523e63920658613b273f09cf112f3e9ad7b7
Christopher Moore <christopher.e.moore@gmail.com>
2016-11-29 16:48:57 +0200

    added test.sh for magnetic field solver

---
+++ MagneticField/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+#dist/osx/debug/test.app/Contents/MacOS/test discreteLaplacian > out.txt
+lua -lmake && dist/linux/debug/MagneticField > out.txt && gnuplot -p -e "set term wx; set style data lines; splot 'out.txt' using 1:2:3; pause -1"

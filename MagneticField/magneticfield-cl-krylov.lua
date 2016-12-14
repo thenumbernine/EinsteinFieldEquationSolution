ef9db7002820ed09403cac6c01753021c89191d0
Christopher Moore <christopher.e.moore@gmail.com>
2016-12-14 09:25:58 +0200

    all looks well with the conj grad OpenCL implementation, except that my problem isn't positive semidefinite and is diverging, so moving on to the conj res OpenCL implementation

--- MagneticField/magneticfield-cl-krylov.lua
+++ MagneticField/magneticfield-cl-krylov.lua
@@ -1,8 +1,6 @@
 #!/usr/bin/env luajit
 
 local ffi = require 'ffi'
-local math = require 'ext.math'
-local range = require 'ext.range'
 
 --[[
 just like in LinearSolvers.ConjugateGradient, except with favor towards reusing object (for the sake of CL buffers)
@@ -35,7 +33,7 @@ local function conjgradCL(args)
 
 	local copy = assert(args.copy)
 	local new = assert(args.new)
-	local free = args.new
+	local free = args.free
 	local dot = assert(args.dot)
 	local mulAdd = assert(args.mulAdd)
 
@@ -88,12 +86,29 @@ local function conjgradCL(args)
 	return x
 end
 
+local function conjresCL(args)
+	local A = assert(args.A)
+	local b = assert(args.b)
+	local x = assert(args.x)
+
+	local MInv = args.MInv
+	local errorCallback = args.errorCallback
+	local epsilon = args.epsilon or 1e-7
+	local maxiter = args.maxiter or 1000
+
+	local copy = assert(args.copy)
+	local new = assert(args.new)
+	local free = args.new
+	local dot = assert(args.dot)
+	local mulAdd = assert(args.mulAdd)
+
+end
+
 -- TODO put this in EinsteinFieldEquation/MagneticField or something?  this doesn't use LinearSolvers just yet
 -- ye olde poisson problem
 -- A x = y, y = rho, x = phi, A = del^2
 
-local size = {2,2,2}
-local env = require 'cl.obj.env'{size=size}
+local env = require 'cl.obj.env'{size={8,8,8}}
 
 local rho = env:buffer{name='rho'}
 local phi = env:buffer{name='phi'}
@@ -110,33 +125,31 @@ local init = program:kernel{
 
 -- jacobi
 local A = program:kernel{
-	argsOut = {phi}, 
-	argsIn = {rho}, 
+	argsOut = {{name='A_phi', buf=true}}, 
+	argsIn = {{name='phi', buf=true}}, 
 	body = require 'template'([[
 	const real4 dx = (real4)(1,1,1,1);
 	
-	real skewSum = 0;
+	real sum = 0;
 	<? for i=0,gridDim-1 do ?>{	
 		int4 iL = i;
 		iL.s<?=i?> = max(i.s<?=i?> - 1, 0);
 		int indexL = indexForInt4(iL);
-		real phiL = phi[indexL];
 		
 		int4 iR = i;
 		iR.s<?=i?> = min(i.s<?=i?> + 1, size.s<?=i?> - 1);
 		int indexR = indexForInt4(iR);
-		real phiR = phi[indexR];
 		
-		skewSum += (phiR + phiL) / (dx.s<?=i?> * dx.s<?=i?>);
+		sum += (phi[indexL] + phi[indexR]) / (dx.s<?=i?> * dx.s<?=i?>);
 	}<? end ?>
 
-	const real diag = -2. * (0
+	sum -= 2. * (0
 <? for i=0,gridDim-1 do ?>
 		+ 1. / (dx.s<?=i?> * dx.s<?=i?>)
 <? end ?>
-	);
+	) * phi[index];
 
-	phi[index] = (rho[index] - skewSum) / diag;
+	A_phi[index] = sum;
 ]], {
 	gridDim = env.gridDim,
 	gridSize = env.size,
@@ -164,23 +177,15 @@ local dot = env:reduce{
 
 init()
 
-local function gpustr(b)
-	local c = b:toCPU()
-	return range(env.volume):map(function(i) return c[i-1] end):map(tostring):concat'\n\t'
-end
-
 conjgradCL{
 	A = function(y, x)
-print(debug.traceback())
-print('A')
-print('x', gpustr(x))
 		A(y.buf, x.buf)
-print('y', gpustr(y))
 	end,
 	b = rho,
 	x = phi,
+	maxiter = env.volume,
 	new = function() return env:buffer() end,
-	free = function(buffer) buffer.buf:free() end,
+	free = function(buffer) buffer.buf:release() end,
 	copy = function(dst, src) 
 		env.cmds:enqueueCopyBuffer{
 			src = src.buf,
@@ -190,51 +195,28 @@ print('y', gpustr(y))
 		}
 	end,
 	dot = function(a,b)
-print(debug.traceback())
-print('a', gpustr(a))
-print('b', gpustr(b))
-print('mul',dotBuf.buf,a.buf,b.buf)
-		mul(
-			assert(dotBuf.buf),
-			assert(a.buf),
-			assert(b.buf))
-print('dotBuf', gpustr(dotBuf))
-		local result = dot()
-print('dot.result',dot.result)
-print('dot.result[0]',dot.result[0])
-print('dot() result',result)
-		result = assert(tonumber(result))
-print('tonumber(result)',result)
-		assert(math.isfinite(result))
-		return result
+		mul(dotBuf.buf, a.buf, b.buf)
+		return dot()
 	end,
 	mulAdd = function(y,a,b,s)
-print(debug.traceback())
-print('mulAdd',y.buf,a.buf,b.buf,s)
-print('a',gpustr(a))
-print('b',gpustr(b))
-print('s',s)
-		mulAdd(
-			assert(y.buf),
-			assert(a.buf),
-			assert(b.buf), 
-			ffi.new('real[1]', s))
-print('y',gpustr(y))
+		mulAdd(y.buf, a.buf, b.buf, ffi.new('real[1]', s))
 	end,
 	errorCallback = function(err, iter)
 		print('err', err, 'iter', iter)
 	end,
 }
 
-print('#x y z rho phi')
+local file = assert(io.open('out.txt', 'wb'))
+file:write('#x y z rho phi\n')
 local rhoCPU = rho:toCPU()
 local phiCPU = phi:toCPU()
 local index = 0
-for i=0,size[1]-1 do
-	for j=0,size[2]-1 do
-		for k=0,size[3]-1 do
-			print(i,j,k,rhoCPU[index],phiCPU[index])
+for i=0,tonumber(env.size.x)-1 do
+	for j=0,tonumber(env.size.y)-1 do
+		for k=0,tonumber(env.size.z)-1 do
+			file:write(i,'\t',j,'\t',k,'\t',rhoCPU[index],'\t',phiCPU[index],'\n')
 			index = index + 1
 		end
 	end
 end
+file:close()
